073c35b6e3df45ff07c1963d33e3cd37ae3dcc19
Andreas Bueckle <abueckle@iu.edu>
2026-08-02 16:52:53 -0400

    Updated body-ui scene

--- 3d-web-work/src/app/app.ts
+++ 3d-web-work/src/app/app.ts
@@ -1,19 +1,26 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BabylonScene } from './components/babylon-scene/babylon-scene';
+import { BodyUiScene } from './components/body-ui-scene/body-ui-scene';
 import { firstValueFrom } from 'rxjs';
 
 const SAMPLE_CSV_URL = 'https://purl.humanatlas.io/ref-organ/kidney-male-right';
 
 @Component({
   selector: 'app-root',
-  imports: [BabylonScene],
+  imports: [BabylonScene, BodyUiScene],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App implements OnInit {
   private readonly http = inject(HttpClient);
   private kidneyPurlJson?: object;
+
+  protected readonly showBabylon = signal(false);
+
+  protected toggleView(): void {
+    this.showBabylon.update((value) => !value);
+  }
 
   ngOnInit(): void {
     this.process_purl_data(SAMPLE_CSV_URL).then((json) => {
